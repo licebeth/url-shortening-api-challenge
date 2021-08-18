7a752aab59ca8f3bc8073691333be19f77cb6cb7
unknown <aliceinwonderland05@live.com>
2021-08-18 11:43:58 +0700

    Fix port and add procfile

--- server.js
+++ server.js
@@ -75,6 +75,12 @@ app.post("/", function(req,res){
   })
 })
 
-app.listen(process.env.PORT || 3000, function() {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server setup success");
 })
